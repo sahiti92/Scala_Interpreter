0ab230d62f479417cf24db663a224bb99f6f87da
Ikshitha1004 <131011355+Ikshitha1004@users.noreply.github.com>
2023-11-06 20:10:30 +0530

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -78,33 +78,33 @@ class Expr {
     }
 }
 
-public class GenerateAst {
-    private static void defineAst(
-            String outputDir, String baseName, List<String> types)
-            throws IOException {
-        // Generate AST classes as before...
-    }
+// public class GenerateAst {
+//     private static void defineAst(
+//             String outputDir, String baseName, List<String> types)
+//             throws IOException {
+//         // Generate AST classes as before...
+//     }
 
-    private static void defineType(
-            PrintWriter writer, String baseName,
- String className, String fieldList) {
- writer.println(" static class " + className + " extends " +
- baseName + " {");
- // Constructor.
- writer.println(" " + className + "(" + fieldList + ") {");
- // Store parameters in fields.
- String[] fields = fieldList.split(", ");
- for (String field : fields) {
- String name = field.split(" ")[1];
- writer.println(" this." + name + " = " + name + ";");
- }
- writer.println(" }");
- // Fields.
- writer.println();
- for (String field : fields) {
-writer.println(" final " + field + ";");
- }
- writer.println(" }");
- }
-    }
-}
+//     private static void defineType(
+//             PrintWriter writer, String baseName,
+//  String className, String fieldList) {
+//  writer.println(" static class " + className + " extends " +
+//  baseName + " {");
+//  // Constructor.
+//  writer.println(" " + className + "(" + fieldList + ") {");
+//  // Store parameters in fields.
+//  String[] fields = fieldList.split(", ");
+//  for (String field : fields) {
+//  String name = field.split(" ")[1];
+//  writer.println(" this." + name + " = " + name + ";");
+//  }
+//  writer.println(" }");
+//  // Fields.
+//  writer.println();
+//  for (String field : fields) {
+// writer.println(" final " + field + ";");
+//  }
+//  writer.println(" }");
+//  }
+//     }
+// }
