7588c2cb8a445b6eec80512fbdeee8d588a1d30f
Ikshitha J <ikshi.thaj0477@gmail.com>
2023-11-08 01:11:14 +0530

    need to change add error methods

---
+++ Crux/Parser.java
@@ -0,0 +1,152 @@
+package Crux;
+
+import java.util.List;
+
+// You should remove this line because the package name should match the folder structure.
+// package Interpreter_Scala.Crux;
+
+import java.sql.Statement;
+import java.util.List;
+import java.util.ArrayList;
+
+class Parser {
+    private static class ParseError extends RuntimeException {};
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private Expressions Expression() {
+        return equality();
+    }
+
+    Expressions parse() {
+        try {
+            return Expression();
+        } catch (ParseError error) {
+            return null;
+        }
+    }
+
+    private Expressions equality() {
+        Expressions expressions = comparison();
+        while (match(Tokentype.BANG_EQUAL, Tokentype.EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expressions right = comparison();
+            expressions = new Expressions.Binary(expressions, operator, right);
+        }
+        return expressions;
+    }
+
+    private boolean match(Tokentype... types) {
+        for (Tokentype type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(Tokentype type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == Tokentype.EOF;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private Expressions comparison() {
+        Expressions expressions = term();
+        while (match(Tokentype.GREATER, Tokentype.GREATER_EQUAL, Tokentype.LESS, Tokentype.LESS_EQUAL)) {
+            Token operator = previous();
+            Expressions right = factor();
+            expressions = new Expressions.Binary(expressions, operator, right);
+        }
+        return expressions;
+    }
+
+    private Expressions term() {
+        Expressions expressions = factor();
+        while (match(Tokentype.MINUS, Tokentype.PLUS)) {
+            Token operator = previous();
+            Expressions right = factor();
+            expressions = new Expressions.Binary(expressions, operator, right);
+        }
+        return expressions;
+    }
+
+    private Expressions factor() {
+        Expressions expressions = unary();
+        while (match(Tokentype.SLASH, Tokentype.STAR)) {
+            Token operator = previous();
+            Expressions right = unary();
+            expressions = new Expressions.Binary(expressions, operator, right);
+        }
+        return expressions;
+    }
+
+    private Expressions unary() {
+        if (match(Tokentype.BANG, Tokentype.MINUS)) {
+            Token operator = previous();
+            Expressions right = unary();
+            return new Expressions.Unary(operator, right);
+        }
+        return primary();
+    }
+
+    private Expressions primary() {
+        if (match(Tokentype.LEFT_PAREN)) {
+            Expressions expressions = Expression();
+            consume(Tokentype.RIGHT_PAREN, "Expect ')' after expression");
+            return new Expressions.Grouping(expressions);
+        }
+        throw error(peek(), "Expect expression");
+    }
+
+    private Token consume(Tokentype type, String message) {
+        if (check(type)) return advance();
+        throw error(peek(), message);
+    }
+
+    private ParseError error(Token token, String message) {
+        Scala.error(token, message); // Assuming 'lox' is a valid reference.
+        return new ParseError();
+    }
+
+//    private void synchronize() {
+//        advance();
+//        while (!isAtEnd()) {
+//            if (previous().type == Tokentype.SEMICOLON) return;
+//        }
+//        switch (peek().type) {
+//            case Tokentype.IF:
+//            case Tokentype.ELSE:
+//            case Tokentype.FOR:
+//            case Tokentype.FUN:
+//            case Tokentype.CLASS:
+//            case Tokentype.WHILE:
+//            case Tokentype.RETURN:
+//            case Tokentype.VAR:
+//            case Tokentype.PRINT:
+//                return;
+//        }
+//        advance();
+//    }
+//}
