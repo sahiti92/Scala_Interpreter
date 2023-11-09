aa24170b2c213a25c1987db25deafc7e669307fb
sahiti92 <145762708+sahiti92@users.noreply.github.com>
2023-11-09 09:35:29 +0530

    Update Parser.java

--- Crux/Parser.java
+++ Crux/Parser.java
@@ -79,6 +79,26 @@ throw error(peek(), message);
 //Lox.error(token, message);
 //return new ParseError();
 //}
+    //    private void synchronize() {
+//        advance();
+//        while (!isAtEnd()) {
+//            if (previous().type == Tokentype.SEMICOLON) return;
+//        }
+//        switch (peek().type) {
+//            case Tokentype.IF:
+//            case Tokentype.ELSE:
+//            case Tokentype.FOR:
+//            case Tokentype.FUN:
+//            case Tokentype.CLASS:
+//            case Tokentype.WHILE:
+//            case Tokentype.RETURN:
+//            case Tokentype.VAR:
+//            case Tokentype.PRINT:
+//                return;
+//        }
+//        advance();
+//    }
+//}
 
     private Expressions comparison() {
         Expressions expressions = term();
@@ -144,23 +164,4 @@ throw error(peek(), message);
         return new ParseError();
     }
 
-//    private void synchronize() {
-//        advance();
-//        while (!isAtEnd()) {
-//            if (previous().type == Tokentype.SEMICOLON) return;
-//        }
-//        switch (peek().type) {
-//            case Tokentype.IF:
-//            case Tokentype.ELSE:
-//            case Tokentype.FOR:
-//            case Tokentype.FUN:
-//            case Tokentype.CLASS:
-//            case Tokentype.WHILE:
-//            case Tokentype.RETURN:
-//            case Tokentype.VAR:
-//            case Tokentype.PRINT:
-//                return;
-//        }
-//        advance();
-//    }
-//}
+
