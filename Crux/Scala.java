a99002b7d49dc34f19ed2c4b38c9b8f42db24883
sahiti92 <145762708+sahiti92@users.noreply.github.com>
2023-11-10 07:17:01 +0530

    Update Scala.java

--- Crux/Scala.java
+++ Crux/Scala.java
@@ -11,7 +11,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Scala {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     public static void main(String[] args) throws IOException {
         if(args.length > 1){
             System.out.println("Usage : scala [script]");
@@ -26,7 +28,8 @@ public class Scala {
     private static void runFile(String path) throws IOException{
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes,Charset.defaultCharset()));
-        if(hadError) System.exit(64);
+        if(hadError) System.exit(65);
+        if(hadRuntimeError) System.exit(70);
     }
     private static void runPrompt() throws IOException{
         InputStreamReader input = new InputStreamReader(System.in);
@@ -53,10 +56,13 @@ public class Scala {
 //if (hadError) return;
 //System.out.println(new AstPrinter().print(expression));
 //}
+        // Next-if (hadError) return;
+//interpreter.interpret(expression);
     }
     static void error(int line, String message){
         report(line,"",message);
     }
+    
     private static void report(int line, String where, String message){
         System.err.println("[line "+line+"] Error"+where+": "+message);
         hadError = true;
@@ -68,5 +74,11 @@ report(token.line, " at end", message);
 report(token.line, " at '" + token.lexeme + "'", message);
 }
 }
+    static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +"\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
+    }
+    
 }
+
 //runtime error and interpreter are left
